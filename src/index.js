43eddecddd9280c0e4c3ca3e5bf5ae78d70ee7fd
iqbalyo <iqbalmustakim734@gmail.com>
2025-02-02 04:32:12 +0700

    sad

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", // Ubah jika perlu
+    origin: ["https://fe-deploy-olive.vercel.app", "http://localhost:5173"]
 }));
 
 // Routes
